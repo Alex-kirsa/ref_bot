34ad125b8a811b57f0ef00f1f0c8b727183a7046
Alex-kirsa <139357971+Alex-kirsa@users.noreply.github.com>
2024-05-16 22:43:31 +0200

    fix2

--- hendlers.py
+++ hendlers.py
@@ -9,31 +9,30 @@ from common import send_mailing
 @dp.callback_query_handler(lambda call: True, state=['*'])
 async def but_filter(call: types.CallbackQuery, state: FSMContext):
     manager = UserManager(call, state)
-    match call.data:
-        case 'send':
-            await call.answer(manager.text.stock.get('send_task'), show_alert=True)
-        case 'delete_task':
-            await call.answer(manager.text.stock.get('del_task'), show_alert=True)
-        case 'send_mailing':
-            data_state = await state.get_data()
-            type_mailing = data_state.get('type_mailing')
-            if type_mailing == 'all':
-                user_ids = manager.db.mailing()
-            else:
-                user_ids = manager.db.mailing(manager.db.active_tasks()[0])
-            await bot.send_message(call.from_user.id, manager.text.stock.get('start_mail').format(users=len(user_ids)))
-            result = await send_mailing(user_ids,
-                                        [data_state.get('create_mailing_photo'), data_state.get('create_mailing_text')])
-            await call.answer(f'Рассылка завершенна. Отправленно {result}/{len(user_ids)}', show_alert=True)
-            await bot.send_message(call.from_user.id, manager.text.stock['admin_start'],
-                                   reply_markup=manager.buttons.get_markup('admin', columns=2))
+    if call.data == 'send':
+        await call.answer(manager.text.stock.get('send_task'), show_alert=True)
+    elif call.data == 'delete_task':
+        await call.answer(manager.text.stock.get('del_task'), show_alert=True)
+    elif call.data == 'send_mailing':
+        data_state = await state.get_data()
+        type_mailing = data_state.get('type_mailing')
+        if type_mailing == 'all':
+            user_ids = manager.db.mailing()
+        else:
+            user_ids = manager.db.mailing(manager.db.active_tasks()[0])
+        await bot.send_message(call.from_user.id, manager.text.stock.get('start_mail').format(users=len(user_ids)))
+        result = await send_mailing(user_ids,
+                                    [data_state.get('create_mailing_photo'), data_state.get('create_mailing_text')])
+        await call.answer(f'Рассылка завершенна. Отправленно {result}/{len(user_ids)}', show_alert=True)
+        await bot.send_message(call.from_user.id, manager.text.stock['admin_start'],
+                               reply_markup=manager.buttons.get_markup('admin', columns=2))
+        return
+    elif call.data == 'done':
+        task = manager.db.active_tasks()
+        user_inviters = manager.db.user_task([call.from_user.id, task[0]])
+        if user_inviters[1] >= task[4]:
+            await bot.send_message(call.from_user.id, task[-1], parse_mode='MarkdownV2')
             return
-        case 'done':
-            task = manager.db.active_tasks()
-            user_inviters = manager.db.user_task([call.from_user.id, task[0]])
-            if user_inviters[1] >= task[4]:
-                await bot.send_message(call.from_user.id, task[-1], parse_mode='MarkdownV2')
-                return
     await manager.msg.answer()
 
 
